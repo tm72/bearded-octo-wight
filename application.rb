66a8a4de35689113597f82316e8fd3eadf634c34
T Murray <murrayt5@msu.edu>
2012-06-13 09:04:29 -0400

    Updated application.rb file

---
+++ application.rb
@@ -0,0 +1,4 @@
+3 Configuring Rails Components
+In general, the work of configuring Rails means configuring the components of Rails, as well as configuring Rails itself. The configuration file config/application.rb and environment-specific configuration files (such as config/environments/production.rb) allow you to specify the various settings that you want to pass down to all of the components.
+
+For example, the default config/application.rb file includes this setting:
